e3816823600e7e5d9ee04271fa94c7e0edc9d94a
veronicapava <veronicapava@gmail.com>
2022-02-25 14:45:09 -0500

    Cambio a JpaRepository

--- src/main/java/co/com/sofka/crud/repository/TodoRepository.java
+++ src/main/java/co/com/sofka/crud/repository/TodoRepository.java
@@ -1,7 +1,9 @@
 package co.com.sofka.crud.repository;
 
 import co.com.sofka.crud.entities.TodoEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
-public interface TodoRepository extends CrudRepository<TodoEntity, Long> {
+public interface TodoRepository extends JpaRepository<TodoEntity, Long> {
+
 }
