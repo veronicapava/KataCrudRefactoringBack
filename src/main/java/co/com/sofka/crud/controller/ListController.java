ab96dc783383edd2d3ef50856860141dc24738bb
veronicapava <veronicapava@gmail.com>
2022-02-25 01:53:25 -0500

    Crea metodos get y post de controladores, funcionando correctamente

--- src/main/java/co/com/sofka/crud/controller/ListController.java
+++ src/main/java/co/com/sofka/crud/controller/ListController.java
@@ -1,11 +1,78 @@
 package co.com.sofka.crud.controller;
 
+import co.com.sofka.crud.entities.ListEntity;
+import co.com.sofka.crud.service.ListService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
 
 @RestController
-@RequestMapping("/list")
+@CrossOrigin(origins = "http://localhost:3000")
+@RequestMapping("/api")
 public class ListController {
+
+
+    @Autowired
+    ListService listservice;
+
+    //Obtenemos todas las listas
+    @GetMapping(value = "/list")
+    public ResponseEntity<List<ListEntity>> getAllList(){
+        List<ListEntity> allList = listservice.getAllList();
+        if(allList.isEmpty()){
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+        return new ResponseEntity<>(allList, HttpStatus.OK);
+    }
+
+
+    //Guardar lista
+    @PostMapping(value = "/list")
+    public ResponseEntity<ListEntity> saveList(@RequestBody ListEntity list){
+        try{
+            ListEntity newList= listservice.saveList(list);
+            return new ResponseEntity<>(newList, HttpStatus.CREATED);
+        } catch(Exception err){
+            return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
+        }
+    }
+
+
+
+
+
+
+
+/*
+
+
+
+    @PutMapping(value = "list")
+    public ListEntity updateList(@RequestBody ListEntity list){
+        try {
+            if(list.getId() != null){
+                return listservice.saveList(list);
+            }
+            throw new RuntimeException("No existe el id para actualizar");
+        } catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+        return list;
+    }
+
+    @DeleteMapping(value = "list/{id}")
+    public void deleteListById(@PathVariable("id")Long id){
+        listservice.deleteList(id);
+    }
+
+    @GetMapping(value = "list/{id}")
+    public ListEntity getListById(@PathVariable("id") Long id){
+        return listservice.getList(id);
+    }
+
+*/
 
 }
--- src/main/java/co/com/sofka/crud/controller/ToDoListController.java
+++ src/main/java/co/com/sofka/crud/controller/ToDoListController.java
@@ -1,33 +1,52 @@
 package co.com.sofka.crud.controller;
 
 import co.com.sofka.crud.entities.TodoEntity;
-import co.com.sofka.crud.service.ListService;
+import co.com.sofka.crud.service.TodoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @CrossOrigin(origins = "http://localhost:3000")
 @RequestMapping("/api")
-public class TodoController {
+public class ToDoListController {
 
     @Autowired
-    private ListService listservice;
+    private TodoService todoService;
 
-    @GetMapping(value = "todos")
-    public Iterable<TodoEntity> list(){
-        return listservice.list();
-    }
-    
-    @PostMapping("todo")
-    public TodoEntity saveTodo(@RequestBody TodoEntity todo){
-        return listservice.saveTodo(todo);
+
+    //Obtenemos todos los todos
+    @GetMapping(value = "/todos")
+    public ResponseEntity<List<TodoEntity>> getAllToDos(){
+      List<TodoEntity> allToDos = todoService.getAllToDos();
+      if(allToDos.isEmpty()){
+          return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+      }
+      return new ResponseEntity<>(allToDos, HttpStatus.OK);
     }
 
+    //Guardar todos
+    @PostMapping("/todo")
+    public ResponseEntity<TodoEntity> saveTodo(@RequestBody TodoEntity todo){
+        System.out.println(todo.toString());
+        try{
+            TodoEntity newToDo= todoService.saveToDo(todo);
+            return new ResponseEntity<>(newToDo, HttpStatus.CREATED);
+        } catch(Exception err){
+            System.out.println(err.getMessage());
+            return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
+        }
+    }
+
+/*
     @PutMapping(value = "todo")
     public TodoEntity updateTodo(@RequestBody TodoEntity todo){
         try {
             if(todo.getId() != null){
-                return listservice.saveTodo(todo);
+                return todoService.saveTodo(todo);
             }
             throw new RuntimeException("No existe el id para actualizar");
         } catch (Exception e){
@@ -38,12 +57,12 @@ public class TodoController {
 
     @DeleteMapping(value = "todo/{id}")
     public void deleteTodoById(@PathVariable("id")Long id){
-        listservice.deleteTodo(id);
+        todoService.deleteTodo(id);
     }
 
     @GetMapping(value = "todo/{id}")
     public TodoEntity getTodoId(@PathVariable("id") Long id){
-        return listservice.getTodo(id);
+        return todoService.getTodo(id);
     }
-
+*/
 }
