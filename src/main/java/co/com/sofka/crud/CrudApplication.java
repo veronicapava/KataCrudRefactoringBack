b1c3b6420e3ef0ff2d8f3712d81aad3a7ec1dd08
veronicapava <veronicapava@gmail.com>
2022-02-27 22:52:54 -0500

    Inicializacion de dependencia

--- src/main/java/co/com/sofka/crud/CrudApplication.java
+++ src/main/java/co/com/sofka/crud/CrudApplication.java
@@ -1,5 +1,6 @@
 package co.com.sofka.crud;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -8,6 +9,12 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class CrudApplication {
+
+	@Bean
+	public ModelMapper modelMapper(){
+		return new ModelMapper();
+	}
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(CrudApplication.class, args);
